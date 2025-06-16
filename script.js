828e54579d7e58a8aad413fe401d62d3bb2c3ea0
PedroComer30 <pedroconsultor30@hotmail.com>
2025-06-16 18:45:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -108,3 +108,9 @@ function botReply(userMessage) {
     addMessage(reply, 'bot');
   }, 500);
 }
+
+const clearBtn = document.getElementById('clearBtn');
+clearBtn.addEventListener('click', () => {
+  const chatMessages = document.getElementById('chatMessages');
+  chatMessages.innerHTML = ''; // Limpa todas as mensagens
+});
